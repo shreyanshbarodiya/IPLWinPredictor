9d439d263a5a5ca356d22365ad61af6b7af6d5c8
shreyansh <shreyansh.barodiya@gmail.com>
2017-04-23 23:25:31 +0530

    Venues average added

--- code/project_v2.py
+++ code/project_v2.py
@@ -28,6 +28,18 @@ df_temp = df_temp.loc[df_temp['inning']==1,:]
 df_temp['inning'] = 2
 del df_temp['total_runs']
 
+df_ground = df_features.groupby(['match_id', 'inning'])['total_runs'].sum().reset_index()
+df_ground = df_ground.merge(df_matches,  left_on = 'match_id', right_on = 'id')
+ground_features  = ['match_id' , 'inning' , 'total_runs', 'city', 'team1', 'winner']
+df_ground = df_ground[ground_features[:]]
+
+df_ground['inning_win'] = df_ground.apply(lambda row: 1 if((row['inning']==1 and row['winner']==row['team1']) or (row['inning']==2 and row['winner']!=row['team1'])) else 0, axis=1)
+del df_ground['team1']
+del df_ground['winner']
+
+df_ground = df_ground.groupby(['inning', 'city'])['total_runs','inning_win'].mean().reset_index()
+df_ground['inning_average'] = df_ground['total_runs']
+del df_ground['total_runs']
 
 df_features = df_features.merge(df_temp, how='left', on = ['match_id', 'inning'])
 df_features['target'].fillna(0, inplace=True)
@@ -50,12 +62,15 @@ df_features = pd.merge(df_features, df_matches[['id','season', 'winner', 'city',
 
 df_features['team1_wins'] = (df_features['team1'] == df_features['winner']).astype('int')
 
+
+df_features = df_features.merge(df_ground,how='left',  on=['city', 'inning'])
+
 df_features.fillna(0, inplace=True)
 
-print(df_features.head())
+print(df_features.head(40))
 
-features = ['inning', 'over', 'total_runs', 'player_dismissed', 'wickets', 'score', 'rem_target','rr', 'rr_diff']
-
+features = ['inning', 'over', 'total_runs', 'player_dismissed', 'wickets', 'score', 'rem_target','rr', 'rr_diff', 'inning_win', 'inning_average' ]
+# features = ['inning', 'over', 'total_runs', 'player_dismissed', 'wickets', 'score', 'rem_target','rr', 'rr_diff', 'inning_win' ]
 
 
 df_val = df_features.ix[df_features.match_id == 557,:]
@@ -63,10 +78,10 @@ df_train = df_features.ix[df_features.match_id != 557,:]
 
 
 
-plt.figure(figsize=(11,11))
-colormap = plt.cm.viridis_r
-sns.heatmap(df_train[features[:]].corr(), vmax=1.0, cmap=colormap, annot=True)
-plt.show()
+# plt.figure(figsize=(11,11))
+# colormap = plt.cm.viridis_r
+# sns.heatmap(df_train[features[:]].corr(), vmax=1.0, cmap=colormap, annot=True)
+# plt.show()
 
 train_X = np.array(df_train[features[:]])
 train_y = np.array(df_train['team1_wins'])
@@ -81,7 +96,7 @@ classifier.fit(train_X, train_y)
 win_prediction = classifier.predict(val_X)
 
 prediction =  classifier.predict_proba(val_X)
-print(prediction[:,1])
+# print(prediction[:,1])
 
 # runs scored in each over vs prediction
 fig = plt.figure()
